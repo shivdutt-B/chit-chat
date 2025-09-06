7258790bba4e71e91f2f2adca763a7ec2f9e8b1b
shivdutt-B <shivdutt059@gmail.com>
2025-09-07 00:25:40 +0530

    add favicon

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-// // Centralized exports for all utils
-// export * from './geminiApi';
-// export * from './constants';
-// export * from './messageHelpers';
-// export * from './summaryService';
-// export * from './smartReplyService';
-// export * from './icebreakerService';
